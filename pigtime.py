2ed4bc909234faf5dda616d8d63f77630ebf7773
dubochao <1158742863@qq.com>
2025-02-12 10:14:32 +0800

    优化代码

--- pigtime.py
+++ pigtime.py
@@ -4,18 +4,15 @@ from datetime import datetime, timezone, timedelta
 def get_pig_timestamp(year=None, month=None, day=None):
     if year and month and day:
         print("传入日期:", year, "年", month, "月", day, "日")
-        hour = year % 10
-        minute = month
-        second = day
-        beijing_time = datetime(year, month, day, hour, minute, second)
     else:
         current_date = datetime.now()
         year, month, day = current_date.year, current_date.month, current_date.day
         print("本地日期:", year, "年", month, "月", day, "日")
-        hour = year % 10
-        minute = month
-        second = day
-        beijing_time = datetime(year, month, day, hour, minute, second)
+        
+    hour = year % 10
+    minute = month
+    second = day
+    beijing_time = datetime(year, month, day, hour, minute, second)
     
     beijing_timezone = timezone(timedelta(hours=8))
     beijing_time = beijing_time.replace(tzinfo=beijing_timezone)
